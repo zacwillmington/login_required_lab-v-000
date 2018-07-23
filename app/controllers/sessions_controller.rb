d0a929c5cf714ca8995668fa25d7ae3943fc7dad
Zac Willmington <zac.willmington@gmail.com>
2018-07-23 22:39:31 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,8 @@ class SessionsController < ApplicationController
       binding.pry
       if current_user == nil || current_user.empty?
           redirect_to '/sessions/new'
+      else
+          redirect_to '/secrets/shows'
       end
-      redirect_to '/secrets/shows'
   end
 end
